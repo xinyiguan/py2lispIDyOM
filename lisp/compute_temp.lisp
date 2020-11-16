024541e3db959afc1cdac6a5419b11aa06204b90
Xinyi Guan <xg792@nyu.edu>
2020-11-16 16:42:26 -0500

    updating jupyter notebook

--- lisp/compute_temp.lisp
+++ lisp/compute_temp.lisp
@@ -1,8 +1,8 @@
 (start-idyom)
 
 (idyom-db:initialise-database)
-(idyom-db:import-data :mid "experiment_history/11-16-20_16.36.04/experiment_input_data_folder/train/" "Train" 66111620163604)
-(idyom-db:import-data :mid "experiment_history/11-16-20_16.36.04/experiment_input_data_folder/test/" "Test" 99111620163604)
-(idyom:idyom 99111620163604 '(cpitch onset) '(cpitch onset) :models :ltm :pretraining-ids '(66111620163604) :k 1 :detail 3 :output-path "experiment_history/11-16-20_16.36.04/experiment_output_data_folder/" :overwrite t)
+(idyom-db:import-data :mid "experiment_history/11-16-20_16.41.45/experiment_input_data_folder/train/" "Train" 66111620164145)
+(idyom-db:import-data :mid "experiment_history/11-16-20_16.41.45/experiment_input_data_folder/test/" "Test" 99111620164145)
+(idyom:idyom 99111620164145 '(cpitch onset) '(cpitch onset) :models :ltm :pretraining-ids '(66111620164145) :k 1 :detail 3 :output-path "experiment_history/11-16-20_16.41.45/experiment_output_data_folder/" :overwrite t)
 
 (quit)
