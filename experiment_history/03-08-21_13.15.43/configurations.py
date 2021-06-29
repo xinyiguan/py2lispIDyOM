3245678580d87b4929d63d3787e81d91558ac779
xinyiguan <xg792@gmail.com>
2021-06-29 15:25:24 +0200

    Jun 28 2021 version update

--- experiment_history/03-08-21_13.15.43/configurations.py
+++
@@ -1,1 +0,0 @@
-{'experiment_history_folder': 'experiment_history/', 'train_test_path': ['./dataset/bach_dataset/'], 'trainp': 0.8, 'experiment_name': 'train_bach_test_bach'}
---
+++ lisp/compute_general.lisp
@@ -0,0 +1,7 @@
+(start-idyom)
+
+(script_import_train)
+(script_import_test)
+(script_configure_model)
+
+(quit)
