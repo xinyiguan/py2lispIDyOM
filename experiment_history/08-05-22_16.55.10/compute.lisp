9487cb7aa2606f6ec2eccc04ba636003804ae8fd
Xinyi Guan <xg792@nyu.edu>
2022-05-09 11:21:48 +0200

    idyom results for melodic priming project (bach chorales) with py2lispIDyOM -V1(pre-interface implement)

--- experiment_history/08-05-22_16.55.10/compute.lisp
+++
@@ -1,4 +0,0 @@
-(start-idyom)
-(idyom-db:import-data :mid "experiment_history/08-05-22_16.55.10/experiment_input_data_folder/test_dataset/" "TEST_DATASET" 66050822165510)
-(idyom:idyom 66050822165510 '(cpitch onset) '(cpitch onset) :k 10 :detail 3 :output-path "experiment_history/08-05-22_16.55.10/experiment_output_data_folder/" :overwrite nil)
-(quit)
---
+++ experiment_history/09-05-22_10.58.07/compute.lisp
@@ -0,0 +1,4 @@
+(start-idyom)
+(idyom-db:import-data :mid "experiment_history/09-05-22_10.58.07/experiment_input_data_folder/test_dataset/" "TEST_DATASET" 66050922105808)
+(idyom:idyom 66050922105808 '(cpitch onset) '(cpitch onset) :models :ltm :k :full :detail 3 :output-path "experiment_history/09-05-22_10.58.07/experiment_output_data_folder/" :overwrite nil)
+(quit)
