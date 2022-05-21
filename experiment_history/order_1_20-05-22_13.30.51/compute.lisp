01c8a046caec3995368f377ef59132c38a31c753
Xinyi Guan <xg792@nyu.edu>
2022-05-21 17:07:40 +0200

    run idyom tutorial updated

--- experiment_history/order_1_20-05-22_13.30.51/compute.lisp
+++
@@ -1,4 +0,0 @@
-(start-idyom)
-(idyom-db:import-data :mid "experiment_history/20-05-22_13.30.51/experiment_input_data_folder/test_dataset/" "TEST_DATASET" 66052022133052)
-(idyom:idyom 66052022133052 '(cpitch onset) '(cpitch onset) :models :ltm :ltmo '(:order-bound 1) :k :full :detail 3 :output-path "experiment_history/20-05-22_13.30.51/experiment_output_data_folder/" :overwrite nil)
-(quit)
